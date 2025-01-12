eb1064dd124d5262f7282482833dfd5cc9378871
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2025-01-13 00:53:07 +0700

    hapus data g penting ehe

--- Database/db.sql
+++ Database/db.sql
@@ -77,39 +77,3 @@ END;
 //
 
 DELIMITER ;
-
-
--- Tambahkan data ke tabel admins
-INSERT INTO admins (nama, profil, username, password, no_hp)
-VALUES
-('Admin Satu', 'admin1.jpg', 'admin1', 'admin123', '081111111111'),
-('Admin Dua', 'admin2.jpg', 'admin2', 'admin123', '081111111112');
-
--- Tambahkan data ke tabel kasir
-INSERT INTO kasir (nama, profil, username, password, no_hp)
-VALUES
-('Kasir Satu', 'kasir1.jpg', 'kasir1', 'kasir123', '081222222222'),
-('Kasir Dua', 'kasir2.jpg', 'kasir2', 'kasir123', '081222222223');
-
--- Tambahkan data ke tabel owners
-INSERT INTO owners (nama, profil, username, password, no_hp)
-VALUES
-('Owner Satu', 'owner1.jpg', 'owner1', 'owner123', '081333333333'),
-('Owner Dua', 'owner2.jpg', 'owner2', 'owner123', '081333333334');
-
--- Tambahkan data ke tabel obat
-INSERT INTO obat (kode, nama, gambar, stok, jenis_obat, kategori, harga)
-VALUES
-('OBT001', 'Paracetamol', 'paracetamol.jpg', 100, 'Tablet', 'Antipiretik', 5000.00),
-('OBT002', 'Amoxicillin', 'amoxicillin.jpg', 50, 'Kapsul', 'Antibiotik', 8000.00),
-('OBT003', 'Antalgin', 'antalgin.jpg', 30, 'Tablet', 'Analgesik', 7000.00),
-('OBT004', 'Loratadine', 'loratadine.jpg', 40, 'Sirup', 'Antihistamin', 15000.00),
-('OBT005', 'Vitamin C', 'vitamin_c.jpg', 200, 'Tablet', 'Vitamin', 3000.00);
-
--- Tambahkan data ke tabel transaksi
-INSERT INTO transaksi (id_kasir, kode_produk, jumlah, total_harga, metode_pembayaran)
-VALUES
-(1, 'OBT001', 10, 50000.00, 'Kes'),
-(1, 'OBT002', 5, 40000.00, 'Transfer'),
-(2, 'OBT003', 3, 21000.00, 'Kes'),
-(2, 'OBT004', 2, 30000.00, 'Transfer');
